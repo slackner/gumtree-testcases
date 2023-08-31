e5f896014872e04a4391ef8faac4c0a7f99cf1d8
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:42:08 +0200

    Testcase JacksonDatabind/16

--- JacksonDatabind/16/src_main_java_com_fasterxml_jackson_databind_introspect_AnnotationMap.java
+++ JacksonDatabind/16/src_main_java_com_fasterxml_jackson_databind_introspect_AnnotationMap.java
@@ -109,7 +109,7 @@ public final class AnnotationMap implements Annotations
             _annotations = new HashMap<Class<? extends Annotation>,Annotation>();
         }
         Annotation previous = _annotations.put(ann.annotationType(), ann);
-        return (previous != null) && previous.equals(ann);
+        return (previous == null) || !previous.equals(ann);
     }
 }
 
