b039dd3b70004880cbada3ac8dea3f71ac8a046e
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:41:57 +0200

    Testcase Jsoup/69

--- Jsoup/69/src_main_java_org_jsoup_nodes_FormElement.java
+++ Jsoup/69/src_main_java_org_jsoup_nodes_FormElement.java
@@ -46,6 +46,11 @@ public class FormElement extends Element {
         return this;
     }
 
+    @Override
+    protected void removeChild(Node out) {
+        super.removeChild(out);
+        elements.remove(out);
+    }
 
     /**
      * Prepare to submit this form. A Connection object is created with the request set up from the form values. You
