5a51b6bf9d81ca9956978cd0df2dc4b262974ba1
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:42:01 +0200

    Testcase Closure/141

--- Closure/141/src_com_google_javascript_jscomp_NodeUtil.java
+++ Closure/141/src_com_google_javascript_jscomp_NodeUtil.java
@@ -599,8 +599,11 @@ public final class NodeUtil {
       case Token.GETELEM:
         return true;
 
+      case Token.FUNCTION:
         // Anonymous functions definitions are not changed by side-effects,
         // and named functions are not part of expressions.
+        Preconditions.checkState(NodeUtil.isAnonymousFunction(n));
+        return false;
     }
 
     for (Node c = n.getFirstChild(); c != null; c = c.getNext()) {
