0620600ac471ec71759cc51acb7954ad4f76f1fa
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:41:59 +0200

    Testcase Jsoup/38

--- Jsoup/38/src_main_java_org_jsoup_parser_HtmlTreeBuilderState.java
+++ Jsoup/38/src_main_java_org_jsoup_parser_HtmlTreeBuilderState.java
@@ -452,7 +452,10 @@ enum HtmlTreeBuilderState {
                         tb.insertEmpty(startTag);
                         tb.framesetOk(false);
                     } else if (name.equals("image")) {
+                        if (tb.getFromStack("svg") == null)
                             return tb.process(startTag.name("img")); // change <image> to <img>, unless in svg
+                        else
+                            tb.insert(startTag);
                     } else if (name.equals("isindex")) {
                         // how much do we care about the early 90s?
                         tb.error(this);
