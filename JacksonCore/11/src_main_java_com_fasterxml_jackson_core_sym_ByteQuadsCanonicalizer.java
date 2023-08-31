0cf47a100f08c400d5e35012bdcee53a63e8a624
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:41:56 +0200

    Testcase JacksonCore/11

--- JacksonCore/11/src_main_java_com_fasterxml_jackson_core_sym_ByteQuadsCanonicalizer.java
+++ JacksonCore/11/src_main_java_com_fasterxml_jackson_core_sym_ByteQuadsCanonicalizer.java
@@ -879,6 +879,7 @@ public final class ByteQuadsCanonicalizer
             _hashShared = false;
             // 09-Sep-2015, tatu: As per [jackson-core#216], also need to ensure
             //    we rehash as needed, as need-rehash flag is not copied from parent
+            _verifyNeedForRehash();
         }
         if (_needRehash) {
             rehash();
