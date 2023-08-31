dc1d1244414c7e0ffca5ca532b430154bb02e4a0
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:41:57 +0200

    Testcase Jsoup/26

--- Jsoup/26/src_main_java_org_jsoup_safety_Cleaner.java
+++ Jsoup/26/src_main_java_org_jsoup_safety_Cleaner.java
@@ -40,6 +40,7 @@ public class Cleaner {
         Validate.notNull(dirtyDocument);
 
         Document clean = Document.createShell(dirtyDocument.baseUri());
+        if (dirtyDocument.body() != null) // frameset documents won't have a body. the clean doc will have empty body.
             copySafeNodes(dirtyDocument.body(), clean.body());
 
         return clean;
