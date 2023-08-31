9dc2533f31b72a3f646afd3ec30c203899c6ff5c
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:42:09 +0200

    Testcase JacksonDatabind/27

--- JacksonDatabind/27/src_main_java_com_fasterxml_jackson_databind_deser_BeanDeserializer.java
+++ JacksonDatabind/27/src_main_java_com_fasterxml_jackson_databind_deser_BeanDeserializer.java
@@ -791,7 +791,7 @@ public class BeanDeserializer
                 // first: let's check to see if this might be part of value with external type id:
                 // 11-Sep-2015, tatu: Important; do NOT pass buffer as last arg, but null,
                 //   since it is not the bean
-                if (ext.handlePropertyValue(p, ctxt, propName, buffer)) {
+                if (ext.handlePropertyValue(p, ctxt, propName, null)) {
                     ;
                 } else {
                     // Last creator property to set?
