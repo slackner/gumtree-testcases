1ff0dd93b485fc36e64007b9a04c185f2b8174d6
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:42:00 +0200

    Testcase Jsoup/40

--- Jsoup/40/src_main_java_org_jsoup_nodes_DocumentType.java
+++ Jsoup/40/src_main_java_org_jsoup_nodes_DocumentType.java
@@ -19,7 +19,6 @@ public class DocumentType extends Node {
     public DocumentType(String name, String publicId, String systemId, String baseUri) {
         super(baseUri);
 
-        Validate.notEmpty(name);
         attr("name", name);
         attr("publicId", publicId);
         attr("systemId", systemId);
