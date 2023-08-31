81d9dfadeefb4abbe0ab6b684d6a3bf1dc828d5d
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:42:06 +0200

    Testcase Closure/160

--- Closure/160/src_com_google_javascript_jscomp_Compiler.java
+++ Closure/160/src_com_google_javascript_jscomp_Compiler.java
@@ -265,6 +265,7 @@ public class Compiler extends AbstractCompiler {
             getDiagnosticGroups().getRegisteredGroups()));
     guards.add(options.getWarningsGuard());
 
+    ComposeWarningsGuard composedGuards = new ComposeWarningsGuard(guards);
 
     // All passes must run the variable check. This synthesizes
     // variables later so that the compiler doesn't crash. It also
@@ -272,13 +273,12 @@ public class Compiler extends AbstractCompiler {
     // about missing variable declarations, we shut that specific
     // error off.
     if (!options.checkSymbols &&
-        (warningsGuard == null || !warningsGuard.disables(
-            DiagnosticGroups.CHECK_VARIABLES))) {
-      guards.add(new DiagnosticGroupWarningsGuard(
+        !composedGuards.enables(DiagnosticGroups.CHECK_VARIABLES)) {
+      composedGuards.addGuard(new DiagnosticGroupWarningsGuard(
           DiagnosticGroups.CHECK_VARIABLES, CheckLevel.OFF));
     }
 
-    this.warningsGuard = new ComposeWarningsGuard(guards);
+    this.warningsGuard = composedGuards;
   }
 
   /**
