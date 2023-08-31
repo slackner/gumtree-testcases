65d5dcfb0a8ac394d1acda413cfbb54dd6abaca3
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:42:15 +0200

    Testcase Gson/15

--- Gson/15/gson_src_main_java_com_google_gson_stream_JsonWriter.java
+++ Gson/15/gson_src_main_java_com_google_gson_stream_JsonWriter.java
@@ -492,7 +492,7 @@ public class JsonWriter implements Closeable, Flushable {
    */
   public JsonWriter value(double value) throws IOException {
     writeDeferredName();
-    if (Double.isNaN(value) || Double.isInfinite(value)) {
+    if (!lenient && (Double.isNaN(value) || Double.isInfinite(value))) {
       throw new IllegalArgumentException("Numeric values must be finite, but was " + value);
     }
     beforeValue();
