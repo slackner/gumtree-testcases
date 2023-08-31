804fd6e38e7d99ae5f2b35f195ebe6878739b155
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:41:59 +0200

    Testcase Jsoup/60

--- Jsoup/60/src_main_java_org_jsoup_select_QueryParser.java
+++ Jsoup/60/src_main_java_org_jsoup_select_QueryParser.java
@@ -35,8 +35,12 @@ public class QueryParser {
      * @return Evaluator
      */
     public static Evaluator parse(String query) {
+        try {
             QueryParser p = new QueryParser(query);
             return p.parse();
+        } catch (IllegalArgumentException e) {
+            throw new Selector.SelectorParseException(e.getMessage());
+        }
     }
 
     /**
