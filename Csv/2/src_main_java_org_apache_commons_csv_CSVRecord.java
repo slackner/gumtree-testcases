e65bc3cbedb22df04da0d8fb48a22bfe5d50bf6b
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:42:18 +0200

    Testcase Csv/2

--- Csv/2/src_main_java_org_apache_commons_csv_CSVRecord.java
+++ Csv/2/src_main_java_org_apache_commons_csv_CSVRecord.java
@@ -82,7 +82,14 @@ public class CSVRecord implements Serializable, Iterable<String> {
                     "No header mapping was specified, the record values can't be accessed by name");
         }
         final Integer index = mapping.get(name);
+        try {
             return index != null ? values[index.intValue()] : null;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(
+                    String.format(
+                            "Index for header '%s' is %d but CSVRecord only has %d values!",
+                            name, index.intValue(), values.length));
+        }
     }
 
     /**
