8e035b9fa89cf4cfa2b39bba38c44243ab6c0879
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:41:57 +0200

    Testcase JacksonCore/23

--- JacksonCore/23/src_main_java_com_fasterxml_jackson_core_util_DefaultPrettyPrinter.java
+++ JacksonCore/23/src_main_java_com_fasterxml_jackson_core_util_DefaultPrettyPrinter.java
@@ -252,6 +252,10 @@ public class DefaultPrettyPrinter
 
     @Override
     public DefaultPrettyPrinter createInstance() {
+        if (getClass() != DefaultPrettyPrinter.class) { // since 2.10
+            throw new IllegalStateException("Failed `createInstance()`: "+getClass().getName()
+                    +" does not override method; it has to");
+        }
         return new DefaultPrettyPrinter(this);
     }
 
