f8d01a2024e0c50ef2381617c796ed2ec73da0e8
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:42:07 +0200

    Testcase Compress/44

--- Compress/44/src_main_java_org_apache_commons_compress_utils_ChecksumCalculatingInputStream.java
+++ Compress/44/src_main_java_org_apache_commons_compress_utils_ChecksumCalculatingInputStream.java
@@ -32,7 +32,13 @@ public class ChecksumCalculatingInputStream extends InputStream {
 
     public ChecksumCalculatingInputStream(final Checksum checksum, final InputStream in) {
 
+        if ( checksum == null ){
+            throw new NullPointerException("Parameter checksum must not be null");
+        }
 
+        if ( in == null ){
+            throw new NullPointerException("Parameter in must not be null");
+        }
 
         this.checksum = checksum;
         this.in = in;
