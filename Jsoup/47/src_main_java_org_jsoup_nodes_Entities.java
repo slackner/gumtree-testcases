805a8592218c723c16a7067991ec74d41f2ea22d
Sebastian Lackner <sebastian.lackner@sysmagine.com>
2023-08-31 16:41:59 +0200

    Testcase Jsoup/47

--- Jsoup/47/src_main_java_org_jsoup_nodes_Entities.java
+++ Jsoup/47/src_main_java_org_jsoup_nodes_Entities.java
@@ -119,7 +119,7 @@ public class Entities {
                         break;
                     case '<':
                         // escape when in character data or when in a xml attribue val; not needed in html attr val
-                        if (!inAttribute)
+                        if (!inAttribute || escapeMode == EscapeMode.xhtml)
                             accum.append("&lt;");
                         else
                             accum.append(c);
